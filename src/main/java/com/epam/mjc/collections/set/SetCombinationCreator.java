15f9bd1f5883c6e7aab6cd61b48611bf3b9feef8
Davidchanz <tovliskatsi@gmail.com>
2023-07-25 17:03:03 +0400

    Collections Task 2 Set completed

--- src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
@@ -1,9 +1,36 @@
 package com.epam.mjc.collections.set;
 
-import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SetCombinationCreator {
     public Set<String> createSetCombination(Set<String> firstSet, Set<String> secondSet, Set<String> thirdSet) {
+        Set<String> result = new TreeSet<>();
+
+        Set<String> firstWithOutThird = new TreeSet<>(firstSet);
+        firstWithOutThird.removeAll(thirdSet);
+
+        Set<String> secondWithOutThird = new TreeSet<>(secondSet);
+        secondWithOutThird.removeAll(thirdSet);
+
+        Set<String> firstAndSecond = new TreeSet<>(firstWithOutThird);
+        firstAndSecond.addAll(secondWithOutThird);
+
+        Set<String> firstUnique = new TreeSet<>(firstWithOutThird);
+        firstUnique.removeAll(secondWithOutThird);
+
+        Set<String> secondUnique = new TreeSet<>(secondWithOutThird);
+        secondUnique.removeAll(firstWithOutThird);
+
+        firstAndSecond.removeAll(firstUnique);
+        firstAndSecond.removeAll(secondUnique);
+
+        Set<String> thirdUnique = new TreeSet<>(thirdSet);
+        thirdUnique.removeAll(firstSet);
+        thirdUnique.removeAll(secondSet);
+
+        result.addAll(firstAndSecond);
+        result.addAll(thirdUnique);
+        return result;
     }
 }
--- src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
@@ -2,8 +2,15 @@ package com.epam.mjc.collections.set;
 
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SubsetOfSquaresCreator {
     public Set<Integer> createSubsetOfSquares(List<Integer> sourceList, int lowerBound, int upperBound) {
+        TreeSet<Integer> result = new TreeSet<>();
+        for(var source: sourceList){
+            result.add(source*source);
+        }
+        result = new TreeSet<>(result.subSet(lowerBound, true, upperBound, true));
+        return result;
     }
 }
